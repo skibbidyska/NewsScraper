f6f3a448495d056cc01e8498c99b881fe3813428
skibbidyska <dustin.wright3@gmail.com>
2018-06-18 18:40:43 -0400

    added button to cards with data-id attr

--- public/app.js
+++ public/app.js
@@ -5,11 +5,19 @@ $.getJSON("/articles", function (data) {
   // }
 
   for (var i = 0; i < data.length; i++) {
-    var card = "<div class='card mt-3'><h5 class='card-header'>" + data[i].title+ "</h5><div class='card-body'><p class='card-text'>" + data[i].snippet + "</p> </div>"
+    var card = "" +
+      "<div class='card mt-3'>" +
+      "<h5 class='card-header'>" + data[i].title+ "</h5>" +
+      "<div class='card-body'>" +
+      "<p class='card-text'>" + data[i].snippet + "</p>" +
+      "</div>" +
+      "<div class='card-footer'>" +
+      "<button type='submit' class='btn btn-primary' data-id=" + data[i]._id + ">Save Article</button>" +
+      "</div>" +
+      "</div>"
 
-    $("#articles").append(card);
+    $("#articles").prepend(card);
 
-  }
-  ;
+  };
 
 });
